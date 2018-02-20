376f29899af26d078c59567ea7774ef6783b0978
Aleksandra_Litvishko <avlitvishko@gmail.com>
2018-02-20 18:31:03 +0400

    something strange

---
+++ src/learn/ForTest.java
@@ -0,0 +1,13 @@
+package learn;
+
+class ForTest {
+	public static void main(String args[]) 
+	throws java.io.IOException {
+		int i;
+		
+		System.out.println("Для остановки нажмите q");
+		
+		for(i = 0;(char) System.in.read() != 'q'; i++) 
+			System.out.println("Прогон # " + i);
+	}
+}
