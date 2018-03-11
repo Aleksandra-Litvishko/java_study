dd4d4a3b26162e462fccfa30c651723e71cff95a
Aleksandra <AVLitvishko@gmail.com>
2018-03-11 21:56:35 +0400

    not important

--- src/learn/DynInit.java
+++ src/learn/DynInit.java
@@ -1,8 +1,8 @@
-package learn;
+Ôªøpackage learn;
 class DynInit {
 	public static void main(String args[]) {
 		double radius = 4, height = 5;
 		double volume = 3.1416 * radius * radius * height;
-		System.out.println("Œ·˙∏Ï: " + volume);
+		System.out.println("–û–±—ä—ë–º: " + volume);
 	}
 }
