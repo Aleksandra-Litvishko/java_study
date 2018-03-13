39117463966162653e4c65b79d9cd7b15a492a8e
Aleksandra <AVLitvishko@gmail.com>
2018-03-13 22:32:56 +0400

    "FindFac" was optimized

--- src/learn/FindFac.java
+++ src/learn/FindFac.java
@@ -2,14 +2,18 @@ package learn;
 
 class FindFac {
 	public static void main(String args[]) {
+		int counter = 0;
 		
 		 for(int i = 2; i <= 100; i++) {
 			System.out.print("Делители " + i + ": ");
 			
-			for(int j = 2; j < i; j++) {
+			for(int j = 2; j <= i / 2; j++) {
 				if ((i % j) == 0) System.out.print(j + " "); 
+				counter++;
 			}
-				System.out.println();
+			
+			System.out.println();
 		}
+		 System.out.println("Количество итераций: " + counter);
 	}
 }
