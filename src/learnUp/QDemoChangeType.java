58977b03da69ecf7658de95c49e3185802ada104
Aleksandra_Litvishko <avlitvishko@gmail.com>
2018-04-04 18:31:53 +0400

    arrays +

--- src/learnUp/QDemoChangeType.java
+++ src/learnUp/QDemoChangeType.java
@@ -1,52 +1,36 @@
 package learnUp;
 
-class Queue {  // класс, РЕАЛИЗУЮЩИЙ очередь для хранения символов
+class Queue1 {  // класс, РЕАЛИЗУЮЩИЙ очередь для хранения символов
 	char q[]; // массив для хранения элементов очереди
-	int q1[];
-	double q2[];
-	
+	int a[];
+	double d[]; 
 	int putloc, getloc; // индексы для вставки и извлечения элементов очереди
 	
-	int putloc1, getloc1;
-	int putloc2, getloc2;
-	
-	Queue (int size) { // конструктор класса
+	Queue1 (int size) { // конструктор класса
 		q = new char[size + 1]; // выделяется память для массива (для очереди)
-		
-		q1 = new int[size + 1];
-		q2 = new double[size + 1];
+		a = new int[size + 1];
+		d = new double[size + 1];
 		
 		putloc = getloc = 0; // инициализация переменных, которые будут выполнять роль индексов для вставки и извлечения данных
 	}
 	
-	void put(char ch) { // метод, которому передаётся значение
+	void put(char ch, int i, double j) { // метод, которому передаётся значение
 		if(putloc == q.length-1) { // если putloc = размер массива - 1, то выводится, что очередь заполнена и происходит возврат из метода оператором return
 			System.out.println(" - очередь заполнена");
 			return;
 		}
 		
 		putloc++; // если putloc < размера массива - 1, то выполняется эта часть метода
-		q[putloc] = ch; // значение, передаваемое методу приравнивается переменной массива с индексом putloc;
-	}
-	
-	void put(int i) {
-		if(putloc1 == q1.length-1) { // если putloc = размер массива - 1, то выводится, что очередь заполнена и происходит возврат из метода оператором return
-			System.out.println(" - очередь заполнена");
-			return;
-		}
-		
-		putloc1++; // если putloc < размера массива - 1, то выполняется эта часть метода
-		q1[putloc1] = i; // значение, передаваемое методу приравнивается переменной массива с индексом putloc;
-	}
-	
-	void put(double d) {
-		if(putloc2 == q2.length-1) { // если putloc = размер массива - 1, то выводится, что очередь заполнена и происходит возврат из метода оператором return
-			System.out.println(" - очередь заполнена");
-			return;
-		}
-		
-		putloc2++; // если putloc < размера массива - 1, то выполняется эта часть метода
-		q2[putloc2] = d; // значение, передаваемое методу приравнивается переменной массива с индексом putloc;
+		if((i == 0) & (j == 0) | (ch != 0)) {
+		    q[putloc] = ch; // значение, передаваемое методу приравнивается переменной массива с индексом putloc;
+		    }
+		if((i != 0) & (ch == 0) | (j == 0)) {
+			a[putloc] = i;
+			}
+		if((ch == 0) & (i == 0) | (j != 0)) {
+	        d[putloc] = j;
+	        }
+	    
 	}
 	
 	char get() { // метод, который извлекает символ из очереди
@@ -60,16 +44,21 @@ class Queue {  // класс, РЕАЛИЗУЮЩИЙ очередь для хранения символов
 	}
 }
 
-class QDemo { 
+class QDemoChangeType { 
 	public static void main(String args[]) {
-		Queue bigQ = new Queue(100); // объявлен объект класса Queue
-		Queue smallQ = new Queue(4); // объявлен ещё один объект класса Queue
+		Queue1 bigQ = new Queue1(100); // объявлен объект класса Queue
+		Queue1 smallQ = new Queue1(4); // объявлен ещё один объект класса Queue
 		char ch;
 		
 		System.out.println("Использование очереди bigQ для сохранения алфавита");
 		for(int i = 0; i < 26; i++) { // помещение буквенных символов в очередь bigQ
-			bigQ.put((char)  ( 'A' + i));
+			bigQ.put('0', 8 + i, 0);
 		}
+		System.out.println("СМОТРИ СЮДА ");
+		
+		for(int i = 0; i < bigQ.a.length; i++ ) {
+			System.out.print(bigQ.a[i] +" "); 
+			}
 		System.out.print("Содержимое очереди bigQ: "); // извлечение буквенных символов из очереди bigQ
 		for(int i = 0; i < 26; i++) {
 			ch = bigQ.get();
@@ -82,7 +71,7 @@ class QDemo {
 		System.out.println("Использование очереди smallQ для генерации ошибок");
 		for(int i = 0; i < 5; i++) {
 			System.out.print("Попытка сохранения " + (char) ('Z' - i));
-			smallQ.put((char) ('Z' - i));
+			smallQ.put((char) ('Z' - i), 0 , 0);
 			System.out.println();
 		}
 		System.out.println();
@@ -92,9 +81,5 @@ class QDemo {
 			ch = smallQ.get();
 			if(ch != (char) 0) System.out.print(ch);
 		}
-		
-		bigQ.put(5);
-		bigQ.put(5.2);
-		
 	}
 }
