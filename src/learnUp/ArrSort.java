a15dda6e690cbea95b7f89572669515e41468e3f
Aleksandra_Litvishko <avlitvishko@gmail.com>
2018-04-10 17:43:59 +0400

    new

--- src/learnUp/ArrSort.java
+++ src/learnUp/ArrSort.java
@@ -3,8 +3,7 @@ package learnUp;
 class ArrSort {
 	public static void main(String args[]) {
 		int arr[] = new int[10];
-		int min, min_i, tmp;
-		min = min_i = tmp = 0;
+		int min, temp = 0;
 		
 		for(int i = 0; i < arr.length; i++) {
 			arr[i] = (int) (Math.random()*100);
@@ -13,20 +12,29 @@ class ArrSort {
 		
 		System.out.println();
 		
-		for(int i = 0; i < arr.length; i++) {
-			arr[i] = min;
-			min_i = i;
-			
-			for(int j = 0; j < arr.length; j++) {
-				if(arr[j] < min) {
-					min = arr[j];
-					min_i = j;
+		for(int i = 0; i < arr.length - 1; i++) {
+			min = i;
+			for(int j = i + 1; j < arr.length; j++) {
+				if(arr[j] < arr[min]) {
+					min = j;
 				}
-			}
-			if(i != min_i) {
-				tmp = arr[i];
+				temp = arr[i];
 				arr[i] = arr[min];
-				arr[min] = tmp;
+				arr[min] = temp;
+			}
+		}
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+		
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = arr.length - 1; j > i; j--) {
+				if(arr[j-1] > arr[j]) {
+					temp = arr[j];
+					arr[j] = arr[j-1];
+					arr[j-1] = temp;
+				}
 			}
 		}
 		for(int i = 0; i < arr.length; i++) {
