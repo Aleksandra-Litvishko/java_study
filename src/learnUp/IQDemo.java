127925158600e7480060820ee7c208ca41c49555
Aleksandra_Litvishko <avlitvishko@gmail.com>
2018-05-17 18:31:59 +0400

    +

---
+++ src/learnUp/IQDemo.java
@@ -0,0 +1,103 @@
+package learnUp;
+
+import Queues.*;
+
+class IQDemo {
+	public static void main(String args[]) {
+		FixedQueue q1 = new FixedQueue(10);
+		CircularQueue q4 = new CircularQueue(10);
+		DynQueue q2 = new DynQueue(5);
+		CircularQueue q3 = new CircularQueue(10);
+		
+		ICharQ iQ;
+		
+		char ch;
+		iQ = q1;
+		
+		for(int i = 0; i < 10; i++) {
+			iQ.put((char) ('A' + i));
+		}
+		
+		System.out.println("Cодержимое фиксированной очереди q1: ");
+		for(int i = 0; i < 10; i++) {
+			ch = iQ.get();
+			System.out.print(ch);
+		}
+		
+		System.out.println();
+		
+		iQ = q4;
+		
+		System.out.println("Cодержимое круговой очереди q4: ");
+		for(int i = 0; i < 10; i++) {
+			ch = iQ.get();
+			System.out.print(ch);
+		}
+		System.out.println();
+		
+		Queues.Queue.copyTo(q1, q4);
+		
+		iQ = q1;
+		
+		System.out.println("Cодержимое фиксированной очереди q1: ");
+		for(int i = 0; i < 10; i++) {
+			ch = iQ.get();
+			System.out.print(ch);
+		}
+		System.out.println();
+		
+		iQ = q4;
+		
+		System.out.println("Cодержимое круговой очереди q4: ");
+		for(int i = 0; i < 10; i++) {
+			ch = iQ.get();
+			System.out.print(ch);
+		}
+		
+		System.out.println();
+		
+		iQ = q2;
+		for(int i = 0; i < 10; i++) {
+			iQ.put((char) ('Z' - i));
+		}
+		
+		System.out.println("Cодержимое динамической очереди: ");
+		for(int i = 0; i < 10; i++) {
+			ch = iQ.get();
+			System.out.print(ch);
+		}
+		
+		System.out.println();
+		
+		iQ = q3;
+		for(int i = 0; i < 10; i++) {
+			iQ.put((char) ('A' + i));
+		}
+		
+		System.out.println("Cодержимое кольцевой очереди: ");
+		for(int i = 0; i < 10; i++) {
+			ch = iQ.get();
+			System.out.print(ch);
+		}
+		
+		System.out.println();
+		
+		// поместить больше символов в кольцевую очередь
+		for(int i = 10; i < 20; i++)  {
+			iQ.put((char) ('A' + i));
+		}
+		
+		System.out.println("Cодержимое кольцевой очереди: ");
+		for(int i = 0; i < 10; i++) {
+			ch = iQ.get();
+			System.out.print(ch);
+		}
+		
+		System.out.println("\nСохранение и использование данных" + " кольцевой очереди: ");
+		for(int i = 0; i < 20; i++)  {
+			iQ.put((char) ('A' + i));
+			ch = iQ.get();
+			System.out.print(ch);
+		}
+	}
+}
