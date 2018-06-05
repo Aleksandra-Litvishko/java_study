85acd99d07429ac049157ace1d7844c37e6012fd
Aleksandra_Litvishko <avlitvishko@gmail.com>
2018-06-05 18:34:14 +0400

    consol output; reading and writing to/from files

---
+++ src/javaautocourse/FileDemo.java
@@ -0,0 +1,9 @@
+package javaautocourse;
+
+import java.io.file;
+
+public class FileDemo {
+	public static void main(String args[]){
+		File file = new File("d:\\Java_Files\\bugs_for_contact_point.txt");
+	}
+}
---
+++ src/learnUp/WriteDemo.java
@@ -0,0 +1,11 @@
+package learnUp;
+
+class WriteDemo { // демонстрация метода System.out.write()
+	public static void main(String args[]) {
+		int b;
+		
+		b = 'S';
+		System.out.write(b); // вывод байтов на экран
+		System.out.write('\n');
+	}
+}
