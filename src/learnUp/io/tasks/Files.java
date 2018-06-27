5c9ded76a9be2f254e1c2bae2e9eef0d7771fbf0
Aleksandra_Litvishko <AVLitvishko@gmail.com>
2018-06-27 19:30:38 +0400

    +

--- src/learnUp/io/tasks/Files.java
+++ src/learnUp/io/tasks/Files.java
@@ -6,35 +6,40 @@ import java.io.*;
 class Files {
 	public static void main(String args[]) {
 
+		int a;
+		FileInputStream infile;
+		FileOutputStream tofile;
+
 		if (args.length != 2) {
 			System.out.println("Использование: FIles откуда куда");
+			return;
 		}
-			int a;
+
+		try {
+			infile = new FileInputStream(args[0]);
+			tofile = new FileOutputStream(args[1]);
 			try {
-				FileInputStream infile = new FileInputStream(args[0]);
-				FileOutputStream tofile = new FileOutputStream(args[1]);
+				do {
+					a = infile.read();
+					if (a == ' ')
+						a = '-';
+					if (a != -1)
+						tofile.write(a);
+				} while (a != -1);
+			} catch (IOException exc) {
+				System.out.println("Возникла ошибка ввода-вывода");
+			} finally {
 				try {
-					do {
-						a = infile.read();
-						if (a == ' ')
-							a = '-';
-						if (a != -1)
-							tofile.write(a);
-					} while (a != -1);
+					if (infile != null)
+						infile.close();
+					if (tofile != null)
+						tofile.close();
 				} catch (IOException exc) {
-					System.out.println("Возникла ошибка ввода-вывода");
-				} finally {
-					try {
-						if (infile != null)
-							infile.close();
-						if (tofile != null)
-							tofile.close();
-					} catch (IOException exc) {
-						System.out.println("Возникла ошибка при закрытии файла");
-					}
+					System.out.println("Возникла ошибка при закрытии файла");
 				}
-			} catch (FileNotFoundException exc) {
-				System.out.println("Файл не найден");
+			}
+		} catch (FileNotFoundException exc) {
+			System.out.println("Файл не найден");
 		}
 	}
 }
