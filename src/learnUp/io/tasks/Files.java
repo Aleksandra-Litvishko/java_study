8edfea54ace8fb277ecf87babe982f6c13c484bb
Aleksandra_Litvishko <AVLitvishko@gmail.com>
2018-06-27 19:28:22 +0400

    fin exercises for I/O

--- src/learnUp/io/tasks/Files.java
+++ src/learnUp/io/tasks/Files.java
@@ -1,50 +1,40 @@
-package learnUp.io.tasks;
+package learnUp.io.tasks; //копирование текстового файла с заменой пробелов дефисами. Используются байтовые потоки.
 
 import java.io.*;
 
+// Для того, чтобы воспользоваться программой, необходимо указать в командной строке имена исходного и целевого файлов. Например, java Files file1 file2
 class Files {
+	public static void main(String args[]) {
 
-	String from, to;
-
-	Files(String from, String to) {
-		this.from = from;
-		this.to = to;
-	}
-	
-	boolean Copy() {
-		int a;
-		FileInputStream infile = null; 
-		FileOutputStream tofile = null; 
-		try {
-			infile = new FileInputStream(from);
-			tofile = new FileOutputStream(to);
-
-			if (infile.read() != -1)
-				do {
-					a = infile.read();
-					if (a == ' ')
-						tofile.write('-');
-					tofile.write(a);
-				} while (a != -1);
-			else
-				return false;
-			return true;
-		} catch (FileNotFoundException exc) {
-			System.out.println("Файл не найден");
-			return false;
-		} catch (IOException exc) {
-			System.out.println("Возникла ошибка ввода-вывода");
-			return false;
-		} finally {
+		if (args.length != 2) {
+			System.out.println("Использование: FIles откуда куда");
+		}
+			int a;
 			try {
-				if (infile != null)
-					infile.close();
-				if (tofile != null)
-					tofile.close();
-			} catch (IOException exc) {
-				System.out.println("Возникла ошибка при закрытии файла");
-				return false;
-			}
+				FileInputStream infile = new FileInputStream(args[0]);
+				FileOutputStream tofile = new FileOutputStream(args[1]);
+				try {
+					do {
+						a = infile.read();
+						if (a == ' ')
+							a = '-';
+						if (a != -1)
+							tofile.write(a);
+					} while (a != -1);
+				} catch (IOException exc) {
+					System.out.println("Возникла ошибка ввода-вывода");
+				} finally {
+					try {
+						if (infile != null)
+							infile.close();
+						if (tofile != null)
+							tofile.close();
+					} catch (IOException exc) {
+						System.out.println("Возникла ошибка при закрытии файла");
+					}
+				}
+			} catch (FileNotFoundException exc) {
+				System.out.println("Файл не найден");
 		}
 	}
 }
--- src/learnUp/io/tasks/ForCopyFiles.java
+++ src/learnUp/io/tasks/ForCopyFiles.java
@@ -2,9 +2,9 @@ package learnUp.io.tasks;
 
 class ForCopyFiles {
 	public static void main(String args[]) {
-		Files newtry = new Files("D:\\helpfile.txt","D:\\newfile1.txt" );
+		FilesWithSymbols newfiles = new FilesWithSymbols(args[0], args[1]);
 		
-		if(newtry.Copy() == false) System.out.println("Копирование не удалось!");
+		if(newfiles.Copy() == false) System.out.println("Копирование не удалось!");
 		else System.out.println("Копирование успешно завершено");
 	}
 }
