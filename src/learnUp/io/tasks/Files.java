09529d9cbd5963632c1b09bb7f530ffcddcbcd57
Aleksandra <AVLitvishko@gmail.com>
2018-06-28 21:58:28 +0400

    +

--- src/learnUp/io/tasks/Files.java
+++ src/learnUp/io/tasks/Files.java
@@ -3,48 +3,41 @@ package learnUp.io.tasks;
 import java.io.*;
 
 class Files {
+	public static void main(String args[]) {
 
-	String from, to;
-
-	Files(String from, String to) {
-		this.from = from;
-		this.to = to;
-	}
-	
-	boolean Copy() {
 		int a;
-		FileInputStream infile = null; 
-		FileOutputStream tofile = null; 
-		try {
-			infile = new FileInputStream(from);
-			tofile = new FileOutputStream(to);
+		FileInputStream infile;
+		FileOutputStream tofile;
 
-			if (infile.read() != -1)
-				do {
-					a = infile.read();
-					if (a == ' ')
-						tofile.write('-');
-					tofile.write(a);
-				} while (a != -1);
-			else
-				return false;
-			return true;
+		if (args.length < 2) {
+			System.out.println("Введите данные в формате: Files from_file to_file");
+			return;
+		}
+		try {
+			infile = new FileInputStream(args[0]);
+			tofile = new FileOutputStream(args[1]);
+			try {
+					do {
+						a = infile.read();
+						if (a == ' ')tofile.write('-');
+						else if(a!= -1) tofile.write(a);
+					} while (a != -1);
+			} catch (IOException exc) {
+				System.out.println("Возникла ошибка ввода-вывода");
+			} finally {
+				try {
+					if (infile != null)
+						infile.close();
+					if (tofile != null)
+						tofile.close();
+				} catch (IOException exc) {
+					System.out.println("Возникла ошибка при закрытии файла");
+				}
+			}
 		} catch (FileNotFoundException exc) {
 			System.out.println("Файл не найден");
-			return false;
-		} catch (IOException exc) {
-			System.out.println("Возникла ошибка ввода-вывода");
-			return false;
-		} finally {
-			try {
-				if (infile != null)
-					infile.close();
-				if (tofile != null)
-					tofile.close();
-			} catch (IOException exc) {
-				System.out.println("Возникла ошибка при закрытии файла");
-				return false;
-			}
+			System.out.println(exc.getMessage());
+			
 		}
 	}
 }
