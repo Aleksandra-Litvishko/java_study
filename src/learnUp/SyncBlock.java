2113b2b4961078d4aeb6dc2a5cd36b033d9086ed
Aleksandra_Litvishko <AVLitvishko@gmail.com>
2018-07-05 18:27:51 +0400

    wait(), notify()

--- src/learnUp/SyncBlock.java
+++ src/learnUp/SyncBlock.java
@@ -1,9 +1,9 @@
 package learnUp;
 
-class SumArray { //использование ключевого слова synchronized для управления доступом
+class SummArray {
 	private int sum;
 
-	synchronized int sumArray(int nums[]) { // метод sumArray() синхронизирован
+	int summArray(int nums[]) { // summArray() не синхронизирован
 		sum = 0; //обнулить сумму
 
 		for (int i = 0; i < nums.length; i++) {
@@ -19,13 +19,13 @@ class SumArray { //использование ключевого слова synchronized для управления дос
 	}
 }
 
-class MyThreadWithSync implements Runnable {
+class MyThreads implements Runnable {
 	Thread thr;
-	static SumArray sa = new SumArray();
+	static SummArray sa = new SummArray();
 	int a[];
 	int answer;
 
-	MyThreadWithSync(String str, int nums[]) {
+	MyThreads(String str, int nums[]) {
 		thr = new Thread(this, str);
 		a = nums;
 		thr.start();
@@ -35,16 +35,19 @@ class MyThreadWithSync implements Runnable {
 		int sum;
 		System.out.println(thr.getName() + " -запуск");
 
-		answer = sa.sumArray(a);
+		synchronized(sa) { 
+		answer = sa.summArray(a);//вызовы метода sumArray() для объекта sa синхронизированы
+		}
 		System.out.println("СУММА для " + thr.getName() + ": " + answer);
 		System.out.println(thr.getName() + " - завершение");
 	}
 }
 
-class Sync {
+class SyncBlock {
 	public static void main(String args[]) {
 		int a[] = { 1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15,16 };
-		MyThreadWithSync mt = new MyThreadWithSync("Child#1", a);
-		MyThreadWithSync mt2 = new MyThreadWithSync("Child#2", a);
+		
+		MyThreads mt = new MyThreads("Child#1", a);
+		MyThreads mt2 = new MyThreads("Child#2", a);
 	}
 }
