c87fa42e7c5c1d79707a746fe934cc000aab3279
Aleksandra_Litvishko <AVLitvishko@gmail.com>
2018-07-16 18:58:43 +0400

    Enums

---
+++ src/learnUp/tasks/TrafficLightDemo.java
@@ -0,0 +1,13 @@
+package learnUp.tasks;
+
+enum TrafficLightColor {
+	GREEN, YELLOW, RED;
+}
+
+class TrafficLightSimulator implements Runnable {
+	private TrafficLightColor tlc;
+}
+
+class TrafficLightDemo {
+
+}
