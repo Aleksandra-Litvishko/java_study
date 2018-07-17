aac3de34d9f9e5256c18bd533ab6234d622e7bdc
Aleksandra_Litvishko <AVLitvishko@gmail.com>
2018-07-17 19:18:55 +0400

    Autopack

---
+++ src/learnUp/AutoBox.java
@@ -0,0 +1,10 @@
+package learnUp;
+
+class AutoBox { // автоупаковка и автораспаковка
+	public static void main(String args[]) {
+		Integer iOb = 100; // автоупаковка и последующа€ автораспаковка значени€ 100
+		int i = iOb;
+		
+		System.out.println(iOb + " " + i);
+	}
+}
---
+++ src/learnUp/Wrap.java
@@ -0,0 +1,10 @@
+package learnUp;
+
+class Wrap { // упаковка и распаковка значений вручную
+	public static void main(String args[]) {
+		Integer iOb = new Integer(100);
+		int a = iOb.intValue();
+
+		System.out.println(a + " " + iOb);
+	}
+}
